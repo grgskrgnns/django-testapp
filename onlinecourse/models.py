dc7a0a57fd86a313e927123f7e3f7442c319bbf9
grgskrgnns <georgios.a.karagiannis@gmail.com>
2021-05-26 09:11:04 +0000

    2nd commit

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -125,13 +125,16 @@ class Question(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=1024, default="This is an answer")
+    is_correct = models.BooleanField(default=True)
+
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    chocies = models.ManyToManyField(Choice)
-#    Other fields and methods you would like to design
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
